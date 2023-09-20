769f9779037aabd9e8e71a329bff444633030ab6
FreddieC19 <freddieclark334@gmail.com>
2023-09-20 12:18:57 -0400

    added noise code and updated output figure to include all 3 images

--- main.py
+++ main.py
@@ -23,21 +23,30 @@ index = int(input("Please enter an integer between 0 and 59999: "))
 input_image, _ = train_set[index]
 input_image = input_image.view(-1)
 
+#add noise to input image
+noise_amt = 0.8
+input_noise = input_image + noise_amt*torch.randn(input_image.shape)
+
 #forward pass the input image through the model to get the reconstructed output
 with torch.no_grad():
-    reconstructed_image = model(input_image.unsqueeze(0)).squeeze(0)
+    reconstructed_image = model(input_noise.unsqueeze(0)).squeeze(0)
 
 #convert the input and reconstructed images to NumPy arrays
 input_image_numpy = input_image.view(28, 28).cpu().numpy()
 reconstructed_image_numpy = reconstructed_image.view(28, 28).cpu().numpy()  # Reshape to (28, 28)
+input_noise_numpy = input_noise.view(28, 28).cpu().numpy()
 
 #display the input and reconstructed images side by side
-plt.figure(figsize=(8, 4))
-plt.subplot(1, 2, 1)
-plt.title("Input Image")
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 3, 1)
+plt.title("Original Input Image")
 plt.imshow(input_image_numpy, cmap='gray')
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
+plt.title("Noisy Input Image")
+plt.imshow(input_noise_numpy, cmap='gray')
+
+plt.subplot(1, 3, 3)
 plt.title("Reconstructed Image")
 plt.imshow(reconstructed_image_numpy, cmap='gray')
 
